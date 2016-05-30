073bfd0f14ab6ddd0076b20a89bd54abf8f1797b
Damian Kosowski <kosowski.d@gmail.com>
2016-05-30 16:10:38 +0200

    Added date parsing with Chronic gem

--- lib/todo.rb
+++ lib/todo.rb
@@ -5,7 +5,7 @@ class TodoItem
   def initialize(description, options={})
     if ["high", "medium", "low", nil].include? options[:priority]
       @description = description
-      @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+      @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
       @priority = options[:priority]  
     else
       raise UdaciListErrors::InvalidPriorityValue, "Invalid priority value passed."

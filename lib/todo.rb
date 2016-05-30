40917ffe30aaabd60d0970b91e328a80aab18900
Damian Kosowski <kosowski.d@gmail.com>
2016-05-30 15:06:41 +0200

    Added errors for invalid type, excess list and priority

--- lib/todo.rb
+++ lib/todo.rb
@@ -6,6 +6,7 @@ class TodoItem
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
+    raise UdaciListErrors::InvalidPriorityValue if !["high", "medium", "low", nil].include? options[:priority]
   end
   
   def details
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,20 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+
   def add(type, description, options={})
     type = type.downcase
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
+    raise UdaciListErrors::InvalidItemType if !["todo", "event", "link"].include? type
   end
+
   def delete(index)
+    raise UdaciListErrors::IndexExceedsListSize if index > @items.length
     @items.delete_at(index - 1)
   end
+  
   def all
     puts "-" * @title.length
     puts @title

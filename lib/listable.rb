5ec7c77410c53a28a9dcacd3dc59ea24fd604df0
Damian Kosowski <kosowski.d@gmail.com>
2016-05-30 14:42:39 +0200

    Moved format_date and format_priority to Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,21 @@ module Listable
 	def format_description(description)
 		"#{description}".ljust(30)
 	end
+
+	def format_date(*dates)
+		dates = dates.map { |date| date.strftime("%D") if date }
+		date_info = dates[0]
+		date_info << " -- " + dates[1] if dates[1]
+		date_info = "No due date" if dates.none? && dates.size == 1
+		date_info = "N/A" if dates.none? && dates.size > 1
+    return date_info
+	end
+
+	 def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
